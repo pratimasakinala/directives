3a6397c22a9dba7e90e01d687f030dc525136f13
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-20 13:06:24 -0700

    creates better highlight directive that uses renderer to apply style changes

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, Renderer2, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[betterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+
+  constructor(private renderer: Renderer2, 
+    private elementRef: ElementRef) { }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+    // forth argument is optional. We can set !important in that argument
+  }
+
+}
