18e177d4b970ff1e82b95eea5bd9405e4dda8e1f
Pratima Sakinala <pratimasakinala@Pratimas-MacBook-Pro.local>
2018-06-20 17:38:46 -0700

    understanding switch & ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,32 @@ export class AppComponent {
   evenNumbers = [2, 4];
   onlyOdd = false;
   colorRed: string = 'red';
+  value: number = 1000;
+
+  ngOnInit() {
+    // if (this.value == 5) {
+    //   console.log('inside first if');
+    // } else if (this.value == 10) {
+    //   console.log('inside second if');
+    // } else if (this.value == 100) {
+    //   console.log('inside third if');
+    // } else {
+    //   console.log('none');
+    // }
+
+    switch(this.value) {
+      case 5:
+        console.log('case 5');
+        break;
+      case 10:
+        console.log('case 10');
+        break;
+      case 100:
+        console.log('case 100');
+        break;
+      default:
+        console.log('none');
+        break;
+    }
+  }
 }
